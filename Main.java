43167747e63dca323eaecb4c15782c24f8eb4ee6
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-08-14 17:51:27 +0530

    print decreasing number

---
+++ Main.java
@@ -0,0 +1,25 @@
+// PRINT DECREASING NUMBER
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printDecreasing(int n){
+
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        printDecreasing(n);
+    }
+
+}
