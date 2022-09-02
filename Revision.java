ac7a2aa09b6338bf258948823ee9ce8032bfb5ea
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-09-02 23:31:40 +0530

    array elements printing using recursion

--- Revision.java
+++ Revision.java
@@ -1,4 +1,6 @@
-package com.company;
+// x power n
+
+/*package com.company;
 
 import java.util.Scanner;
 
@@ -18,4 +20,33 @@ public class Revision {
         int n=scn.nextInt();
         System.out.println(power(x,n));
     }
+}*/
+
+//----------------------------------------------------------------------------------------------------------
+
+// PRINT ELEMENTS OF ARRAY
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Revision{
+
+    public static void printArr(int arr[],int idx){
+        if(idx== arr.length) return;
+        System.out.println(arr[idx]);
+        printArr(arr,idx+1);
+    }
+
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        printArr(arr,0);
+    }
 }
+
+
