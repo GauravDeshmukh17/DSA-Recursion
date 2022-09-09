edb2786ff259d393728edf6204faeb594a5f54b4
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-09-09 15:19:07 +0530

    tower of hanoi

--- Main.java
+++ Main.java
@@ -287,7 +287,7 @@ public class Main{
 
 // MAXIMUM OF ARRAY(METHOD-2)
 
-package com.company;
+/*package com.company;
 
 import java.util.*;
 
@@ -313,6 +313,40 @@ public class Main{
         }
         System.out.println(maxOfArray(arr,0));;
     }
+}*/
+
+//---------------------------------------------------------------------------------------------------------
+
+// TOWER OF HANOI
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Main{
+
+                       //        source destination helper
+    public static void toh(int n,int t1,int t2,int t3){
+
+        if(n==0) return;
+
+        toh(n-1,t1,t3,t2);
+
+        System.out.println(t1+" ---> "+t2);
+
+        toh(n-1,t3,t2,t1);
+    }
+
+    public static void main(String args[]){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int t1=scn.nextInt();
+        int t2=scn.nextInt();
+        int t3=scn.nextInt();
+
+        toh(n,t1,t2,t3);
+    }
 }
+
 
 
