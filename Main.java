21271e03382b0a69d04a1df028f5e86c42201947
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-09-21 13:41:50 +0530

    print sub-strings

--- Main.java
+++ Main.java
@@ -655,7 +655,7 @@ public class Main{
 
 // GET KPC
 
-package com.company;
+/*package com.company;
 
 import java.util.*;
 
@@ -697,8 +697,43 @@ public class Main{
         }
 
     }
+}*/
+
+//----------------------------------------------------------------------------------------------------------
+
+// RECURSION ON THE WAY
+// PRINT SUB-STRINGS
+
+package com.company;
+
+import java.util.*;
+
+public class Main{
+
+    public static void printSS(String str,String asf){
+        if(str.length()==0){
+            if(asf==""){
+                asf="-";
+            }
+            System.out.println(asf);
+            return;
+        }
+
+        char ch=str.charAt(0);
+        String ros=str.substring(1);
+
+        // Include character
+        printSS(ros,asf+ch);
+
+        //Exclude character
+        printSS(ros,asf);
+    }
+
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+
+        printSS(str,"");
+    }
 }
-
-
-
 
