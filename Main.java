084712bdee4fe7f03b738880253caa0f56039eed
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-10-21 15:19:32 +0530

    bubble sort

--- Main.java
+++ Main.java
@@ -1286,12 +1286,12 @@ public class Main{
     }
 }*/
 
-//--------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
 
 // RECURSION AND BACKTRACKING
 // FLOOD FILL (WITH OBSTACLE)
 
-package com.company;
+/*package com.company;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -1353,6 +1353,46 @@ public class Main{
         }
     }
 
+}*/
+
+//-----------------------------------------------------------------------------------------------------------
+
+// BUBBLE SORT
+
+package com.company;
+
+import java.util.*;
+
+public class Main{
+
+    public static int[] bubbleSort(int[] arr){
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        int[] bs=bubbleSort(arr);
+        for(int i=0;i<bs.length;i++){
+            System.out.print(bs[i]+" ");
+        }
+    }
 }
 
 
