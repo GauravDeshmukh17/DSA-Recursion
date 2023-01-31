d7e4be24442c59b5da69c69f1b4b917f142a3e7b
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-01-31 16:26:50 +0530

    print increasing

--- Revision.java
+++ Revision.java
@@ -161,7 +161,7 @@ public class Revision{
 
  // PRINT DECREASING NUMBER
 
- package com.company;
+ /*package com.company;
  import java.util.*;
 
  public class Revision{
@@ -178,7 +178,29 @@ public class Revision{
 
          printDecreasing(n);
      }
-}
+}*/
 
+//----------------------------------------------------------------------------------------------------------
+
+ // PRINT INCREASING
+
+ package com.company;
+ import java.util.*;
+
+ public class Revision{
+
+     public static void printIncreasing(int n){
+         if(n==0) return;
+         printIncreasing(n-1);
+         System.out.println(n);
+     }
+
+     public static void main(String[] args) {
+         Scanner scn=new Scanner(System.in);
+         int n=scn.nextInt();
+
+         printIncreasing(n);
+     }
+ }
 
 
