9b0eb076312a21f76f85748ab56ff49626051e4c
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-02-01 13:40:59 +0530

    :alien: dispaly reverse array

--- Revision.java
+++ Revision.java
@@ -257,4 +257,33 @@ public class Revision{
 
 //------------------------------------------------------------------------------------------------------
 
+// DISPLAY REVERSE ARRAY USING RECURSION
+
+package com.company;
+
+import java.util.*;
+
+public class Revision{
+
+    public static void displayReverseArray(int[] arr,int idx){
+        if(idx==arr.length) return;
+        displayReverseArray(arr,idx+1);
+        System.out.println(arr[idx]);
+    }
+
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        displayReverseArray(arr,0);
+    }
+}
+
+
+
+
 
